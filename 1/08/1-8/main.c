51ae9d79bc27c09eccea7f2e342f90a2b9a40079
JPLACLAU <laclaujeanpaul@gmail.com>
2023-07-01 17:57:34 -0300

    1-8

---
+++ 1/08/1-8/main.c
@@ -0,0 +1,37 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* count lines in input */
+
+int main()
+{
+    printf("Exercise 1-8");
+    printf("\n");
+    printf("Write a program to count blanks, tabs, and newlines");
+    printf("\n");
+    int c, blanks, tabs, nl;
+
+    nl = 0;
+    while ((c = getchar()) !=EOF)
+    {
+        if ( c == ' ')
+        ++blanks;
+        if ( c == '\t')
+        ++tabs;
+        if ( c == '\n')
+        ++nl;
+
+
+    }
+    printf("blanks: %d\n", blanks);
+    printf("\n");
+    printf("tabs: %d\n", tabs);
+    printf("\n");
+    printf("nl: %d\n", nl);
+    printf("\n");
+
+
+
+}
